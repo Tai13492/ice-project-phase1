b13908803f9687a813917ac1b01f5b79fc0b9228
Unknown <friendship045@gmail.com>
2019-02-15 08:36:46 +0700

    feat: add UI of share lockers and find lockers

--- src/pages/lockers/index.js
+++ src/pages/lockers/index.js
@@ -1,8 +1,73 @@
 import React from "react";
+import { NavBar, Card, WingBlank, WhiteSpace } from "antd-mobile";
+
+const lockers = [
+  {
+    faculty: "Faculty of Engineering",
+    imgSrc:
+      "https://gw.alipayobjects.com/zos/rmsportal/MRhHctKOineMbKAZslML.jpg",
+    available: 12
+  },
+  {
+    faculty: "Faculty of Arts",
+    imgSrc:
+      "https://gw.alipayobjects.com/zos/rmsportal/MRhHctKOineMbKAZslML.jpg",
+    available: 8
+  },
+  {
+    faculty: "Faculty of Architecture",
+    imgSrc:
+      "https://gw.alipayobjects.com/zos/rmsportal/MRhHctKOineMbKAZslML.jpg",
+    available: 4
+  },
+  {
+    faculty: "Faculty of Economy",
+    imgSrc:
+      "https://gw.alipayobjects.com/zos/rmsportal/MRhHctKOineMbKAZslML.jpg",
+    available: 24
+  },
+  {
+    faculty: "Faculty of Arts",
+    imgSrc:
+      "https://gw.alipayobjects.com/zos/rmsportal/MRhHctKOineMbKAZslML.jpg",
+    available: 8
+  },
+  {
+    faculty: "Faculty of Architecture",
+    imgSrc:
+      "https://gw.alipayobjects.com/zos/rmsportal/MRhHctKOineMbKAZslML.jpg",
+    available: 4
+  },
+  {
+    faculty: "Faculty of Economy",
+    imgSrc:
+      "https://gw.alipayobjects.com/zos/rmsportal/MRhHctKOineMbKAZslML.jpg",
+    available: 24
+  }
+];
 
 const Lockers = () => {
-  return <h1> This is lockers from A and Tai, A is done with his work</h1>;
-  //   const fetchLockers =
+  return (
+    <div>
+      <NavBar mode="dark"> Find Lockers </NavBar>
+      <WingBlank size="lg">
+        <WhiteSpace size="lg" />
+        {lockers.map(({ faculty, imgSrc, available }) => {
+          return (
+            <React.Fragment>
+              <Card>
+                <Card.Header title={faculty} thumb={imgSrc} />
+                <Card.Body>
+                  <div> Available: {available}</div>
+                </Card.Body>
+              </Card>
+              <WhiteSpace size="lg" />
+            </React.Fragment>
+          );
+        })}
+      </WingBlank>
+    </div>
+  );
 };
 
 export default Lockers;
--- src/pages/sharelockers/index.js
+++ src/pages/sharelockers/index.js
@@ -1,7 +1,50 @@
 import React from "react";
+import { NavBar } from "antd-mobile";
+import { WhiteSpace } from "antd-mobile";
+import { Card, Button } from "antd";
+import facultyOfEngineering from "../../assets/facultyOfEngineeringSign.jpeg";
 
 const ShareLockers = () => {
-  return <h1> This is share lockers </h1>;
+  return (
+    <div>
+      <NavBar mode="dark"> Share Lockers </NavBar>
+      <WhiteSpace size="lg" />
+      <Card
+        style={{ width: "84vw", marginLeft: "8vw", marginRight: "8vw" }}
+        cover={<img alt="example" src={facultyOfEngineering} />}
+        actions={[
+          <Button type="primary" size="large" icon="team">
+            {" "}
+            Share{" "}
+          </Button>
+        ]}
+      >
+        <Card.Meta
+          title="Building 2, Faculty of Engineering"
+          description="Deposited since February 14, 9:20"
+        />
+      </Card>
+      <WhiteSpace size="lg" />
+      <Card
+        style={{ width: "84vw", marginLeft: "8vw", marginRight: "8vw" }}
+        cover={<img alt="example" src={facultyOfEngineering} />}
+        actions={[
+          <Button type="primary" size="large" icon="team">
+            {" "}
+            Share{" "}
+          </Button>
+        ]}
+      >
+        <Card.Meta
+          title="100 years Building, Faculty of Engineering"
+          description="Deposited since February 14, 14:30"
+        />
+      </Card>
+      <WhiteSpace size="lg" />
+    </div>
+  );
 };
 
 export default ShareLockers;
+
+//https://upload.wikimedia.org/wikipedia/commons/0/03/Chula_eng1bldg.jpg
