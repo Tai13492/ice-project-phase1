f2b633da8673b0484279235eac3160708c93a5f5
Tai13492 <friendship045@gmail.com>
2019-03-12 22:46:04 +0700

    fix: react not build properly

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("/locker-status", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
 app.listen(port, () => {
