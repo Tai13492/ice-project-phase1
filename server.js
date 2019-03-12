4d40a6e0ea1116f5da3e02f63b07e21a2fa840e5
Tai13492 <friendship045@gmail.com>
2019-03-12 22:53:37 +0700

    fix: fix missing express static in build

--- server.js
+++ server.js
@@ -21,9 +21,12 @@ app.get("/locker-status", (req, res) => {
   res.json({ status: isOpen });
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/build/index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`server starting at port ${port}`);
