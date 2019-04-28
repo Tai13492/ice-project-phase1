118fda0de2b0e6822287552668a3b9ec10edfd49
Tai <friendship045@gmail.com>
2019-04-28 12:31:20 +0700

    remove unnessary logs

--- client/src/auth/index.js
+++ client/src/auth/index.js
@@ -9,38 +9,33 @@ import Axios from "axios";
 import { END_POINT } from "../index";
 
 class Auth extends React.Component {
-  state = {
-    res: ""
-  };
   async componentDidMount() {
     const { setAuthentication, setTokenAndExpiration } = this.props;
     initAxiosErrorHandling(deleteTokenAndExpiration);
     const idToken = localStorage.getItem("idToken");
     const expireIn = localStorage.getItem("expireIn");
-    // if (idToken) {
-    //   try {
-    //     const res = await Axios.post("/auth/myToken/line", {
-    //       lineToken: idToken
-    //     });
-    //     if (res) {
-    //       // this.setState({ res });
-    //       localStorage.setItem("token", res.data.token);
-    //       setAuthentication(true);
-    //       setTokenAndExpiration(idToken, expireIn);
-    //       Axios.defaults.headers.common["Authorization"] =
-    //         "Bearer " + res.data.token;
-    //     }
-    //     this.setState({ res });
-    //   } catch (error) {
-    //     console.log(error);
-    //     throw error;
-    //   }
-    // } else {
-    window.location.href = END_POINT + "/auth/lineLoginPage";
-    //}
+    if (idToken) {
+      try {
+        const res = await Axios.post("/auth/myToken/line", {
+          lineToken: idToken
+        });
+        if (res) {
+          localStorage.setItem("token", res.data.token);
+          setAuthentication(true);
+          setTokenAndExpiration(idToken, expireIn);
+          Axios.defaults.headers.common["Authorization"] =
+            "Bearer " + res.data.token;
+        }
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    } else {
+      window.location.href = END_POINT + "/auth/lineLoginPage";
+    }
   }
   render() {
-    return <h1> Loading... {JSON.stringify(this.state.res)}</h1>;
+    return <h1> Loading...</h1>;
   }
 }
 
